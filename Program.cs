ce759cb025bc31a776f78571b17cdc72e2cc0f30
Porosyatko <valera2020657632578@gmail.com>
2024-11-29 10:33:18 +0300

    Сохранение

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace GetStartedDebugging
+﻿using System;
+
+class ArrayExample
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
         {
-            Console.WriteLine("Hello, World!");
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
         }
+        Console.ReadKey();
+    }
+
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
     }
 }
